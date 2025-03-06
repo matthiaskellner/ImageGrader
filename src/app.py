9e8e54a72cb4c3853693c80411a238591d1ab264
matthiaskellner <matthiasj.kellner@gmail.com>
2025-03-06 15:58:13 +0100

    Add ImageGrader app

---
+++ src/app.py
@@ -0,0 +1,196 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QLineEdit
+from PyQt5.QtWidgets import QSizePolicy
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from glob import glob
+import qimage2ndarray
+import tifffile
+import pandas as pd
+import os
+
+GRADES = {
+    "1": "GOOD",
+    "2": "MEDIUM",
+    "3": "BAD"
+}
+
+def _get_images(base_dir, ending, last):
+    images = sorted(glob(f"{base_dir}/*.{ending}"))
+    try:
+        # resume if grading already started
+        idx = images.index(last) + 1
+    except:
+        idx = 0
+    return images[idx:]
+
+class ImageWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("Image Viewer")
+        self.resize(1200, 700)
+        main_widget = QWidget()
+        main_layout = QHBoxLayout()
+        
+        self.input_dir = None
+        self.ending = None
+        self.output_dir = None
+        self.csv_file = None
+        self.images = None
+        self.current_idx = None
+        self.df = None
+
+        self.image_label = QLabel()
+        self.image_label.setAlignment(Qt.AlignCenter)
+        self.image_label.setSizePolicy(QSizePolicy.Policy.Ignored , QSizePolicy.Policy.Ignored)
+        self.image_caption_label = QLabel()
+        self.image_caption_label.setAlignment(Qt.AlignCenter)
+        
+        center_layout = QVBoxLayout()
+        center_layout.addWidget(self.image_label, 10)
+        center_layout.addWidget(self.image_caption_label, 0)
+        center_layout.addStretch(1)
+        center_widget = QWidget()
+        center_widget.setLayout(center_layout)
+
+        self.text_label_input = QLabel("Select the input directory using the button below.")
+        self.text_label_input.setAlignment(Qt.AlignCenter)
+        self.text_label_input.setWordWrap(True)
+        self.dir_button_input = QPushButton("Select Input Directory")
+        self.dir_button_input.clicked.connect(self.select_directory("input"))
+
+        self.text_label_output = QLabel("Select the output directory using the button below.")
+        self.text_label_output.setAlignment(Qt.AlignCenter)
+        self.text_label_output.setWordWrap(True)
+        self.dir_button_output = QPushButton("Select Output Directory")
+        self.dir_button_output.clicked.connect(self.select_directory("output"))
+
+        self.ending_textbox = QLineEdit()
+        self.ending_textbox.setPlaceholderText("Enter your file ending here...")
+        self.ending_button = QPushButton("Confirm File Ending")
+        self.ending_button.clicked.connect(self.set_ending)
+
+        self.start_button = QPushButton("Start Grading")
+        self.start_button.setEnabled(False)
+        self.start_button.clicked.connect(self.start_grading)
+
+        self.instruction_label = QLabel("\nRate the images by pressing numbers:\n1=GOOD, 2=MEDIUM, 3=BAD\n\nYou can undo the last grade by pressing 'e' or '0'")
+        self.instruction_label.setAlignment(Qt.AlignCenter)
+        self.instruction_label.setWordWrap(True)
+        
+        side_layout = QVBoxLayout()
+        side_layout.addStretch()
+        side_layout.addWidget(self.text_label_input)
+        side_layout.addWidget(self.dir_button_input)
+        side_layout.addWidget(self.text_label_output)
+        side_layout.addWidget(self.dir_button_output)
+        side_layout.addWidget(self.ending_textbox)
+        side_layout.addWidget(self.ending_button)
+        side_layout.addWidget(self.start_button)
+        side_layout.addWidget(self.instruction_label)
+        side_layout.addStretch()
+        side_widget = QWidget()
+        side_widget.setLayout(side_layout)
+        side_widget.setFixedWidth(350)
+
+        main_layout.addWidget(center_widget)
+        main_layout.addWidget(side_widget)
+        main_widget.setLayout(main_layout)
+        self.setCentralWidget(main_widget)
+    
+    def __exit__(self):
+        self.df.to_csv(self.csv_file, header=not os.path.exists(self.csv_file), index=False)
+    
+    def select_directory(self, type):
+        def _select_directory():
+            dir = QFileDialog.getExistingDirectory(self, "Select Directory")
+            if dir:
+                if type == "input":
+                    self.text_label_input.setText(f"Selected Input Directory:\n{dir}")
+                    self.input_dir = dir
+                else:
+                    self.text_label_output.setText(f"Selected Output Directory:\n{dir}")
+                    self.output_dir = dir
+                
+                if self.input_dir is not None and self.output_dir is not None and self.ending is not None:
+                    self.start_button.setEnabled(True)
+        return _select_directory
+    
+    def set_ending(self):
+        ending = self.ending_textbox.text()
+        if ending:
+            self.ending = ending[1:] if ending[0] == '.' else ending
+            if self.input_dir is not None and self.output_dir is not None and self.ending is not None:
+                self.start_button.setEnabled(True)
+
+    def start_grading(self):
+        self.csv_file = f"{self.output_dir}/{self.input_dir.split('/')[-1]}_GRADES.csv"
+        if os.path.exists(self.csv_file):
+            self.df = pd.read_csv(self.csv_file, usecols=["img_path", "grade"])
+        else:
+            self.df = pd.DataFrame(columns=["img_path", "grade"])
+        try:
+            last_image = self.df.iloc[-1]["img_path"]
+        except IndexError:
+            last_image = None
+        self.images = _get_images(self.input_dir, self.ending, last_image)
+        self.current_idx = 0
+        self.update_image()
+
+    def update_image(self):
+        if self.images is None:
+            return
+        
+        if self.current_idx >= len(self.images):
+            self.image_label.setText("No images found or none left to grade!")
+            self.image_caption_label.setText("")
+        else:
+            image = tifffile.imread(self.images[self.current_idx])
+            pixmap = QPixmap.fromImage(qimage2ndarray.array2qimage(image))
+
+            if not pixmap.isNull():
+                scaled_pixmap = pixmap.scaled(self.image_label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+                self.image_label.setPixmap(scaled_pixmap)
+                self.image_caption_label.setText(self.images[self.current_idx].split('/')[-1])
+
+    def resizeEvent(self, event):
+        self.update_image()
+        super().resizeEvent(event)
+    
+    def _grade(self, event):
+        print(f"{self.images[self.current_idx].split('/')[-1]}    {GRADES[event.text()]}")
+        new_data = pd.DataFrame([{"img_path": self.images[self.current_idx], "grade": int(event.text())}])
+        self.df = pd.concat([self.df, new_data], ignore_index=True)
+        self.df.to_csv(self.csv_file, index=False)
+        self.current_idx += 1
+        self.update_image()
+    
+    def _remove_grade(self):
+        try:
+            print(f"Removed grade for  {self.df.iloc[-1]['img_path'].split('/')[-1]}")
+            self.df = self.df[:-1]
+            self.df.to_csv(self.csv_file, index=False)
+            last_image = self.df.iloc[-1]["img_path"]
+        except IndexError:
+            last_image = None
+        self.images = _get_images(self.input_dir, self.ending, last_image)
+        self.current_idx = 0
+        self.update_image()
+
+    def keyPressEvent(self, event):
+        if event.text() in {"1", "2", "3"}:
+            if self.images is not None and self.current_idx < len(self.images):
+                self._grade(event)
+        elif event.text() in {"0", "e"}:
+            self._remove_grade()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    try:
+        window = ImageWindow()
+        window.show()
+        app.exec_()
+    except Exception as e:
+        print(f"Something went wrong! {str(e)}")
